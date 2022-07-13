b524e6c40f82a2a38c6d898b0b2b0b8feb9c3db4
Duy-Kun <91863679+sherlockduy4869@users.noreply.github.com>
2022-07-13 17:30:50 +0700

    Feat: create form to insert customer data

---
+++ script.js
@@ -0,0 +1,14 @@
+$(document).ready(function(){
+    $('#button_insert').on('click', function(){
+        var full_name = $('#full_name').val();
+        var phone_number = $('#phone_number').val();
+        var address = $('#address').val();
+        var email = $('#email').val();
+        var note = $('#note').val();
+
+        $.ajax({
+
+        });
+
+    });
+})
